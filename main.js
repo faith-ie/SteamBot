76ecda7b509b6a4ff172df1e43ed538322a96c7b
dvsazei <danielviola4457@gmail.com>
2020-03-19 16:23:38 -0400

    First Commit

---
+++ main.js
@@ -0,0 +1,12 @@
+const SteamUser = require('steam-user')
+const creds = require('./creds.json')
+const Client = new SteamUser()
+Client.logOn(creds)
+Client.on('loggedOn', () => {
+  console.log('logged into steam owo')
+  Client.setPersona(SteamUser.Online)
+  console.log('I\' offline')
+  console.log('Marked as online owo')
+  Client.gamesPlayed(creds)
+  console.log('Playing ze game')
+})
